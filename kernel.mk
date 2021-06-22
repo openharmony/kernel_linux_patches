3132d252a42a6be5faea8d16f75e1f2bfb0ef106
yuejianjun <yuejianjun@outlook.com>
2021-06-22 15:21:47 +0800

    Using small patch
    Signed-off-by: yuejianjun <yuejianjun@outlook.com>
    Change-Id: Id6196bdf44be49ab2c6774f8fdbabd03b5e3a3cb

--- kernel.mk
+++ kernel.mk
@@ -59,31 +59,18 @@ KERNEL_MAKE := \
     PATH="$(BOOT_IMAGE_PATH):$$PATH" \
     $(KERNEL_PREBUILT_MAKE)
 
-ifeq ($(PRODUCT_NAME), Hi3516DV300)
-HI3516DV300_PATCH_DIR := $(OHOS_BUILD_HOME)/kernel/linux/patches/linux-4.19/hi3516dv300_small_patch
-HI3516DV300_PATCH_FILE := $(HI3516DV300_PATCH_DIR)/hi3516dv300_small.patch
-HI3516DV300_PATCH_FILE := $(OHOS_BUILD_HOME)/device/hisilicon/hi3516dv300/sdk_linux/open_source/linux/hisi_linux-4.19_hos_l2.patch
-HDF_PATCH_FILE := $(HI3516DV300_PATCH_DIR)/hdf.patch
-DFX_PATCH_FILE := $(HI3516DV300_PATCH_DIR)/dfx.patch
-KERNEL_IMAGE_FILE := $(KERNEL_SRC_TMP_PATH)/arch/arm/boot/uImage
-export HDF_PROJECT_ROOT=$(OHOS_BUILD_HOME)/
-export PRODUCT_PATH=vendor/hisilicon/hispark_taurus_linux
 
-$(KERNEL_IMAGE_FILE):
-	$(hide) echo "build kernel..."
-	$(hide) rm -rf $(KERNEL_SRC_TMP_PATH);mkdir -p $(KERNEL_SRC_TMP_PATH);cp -arfL $(KERNEL_SRC_PATH)/. $(KERNEL_SRC_TMP_PATH)/
-	$(hide) cd $(KERNEL_SRC_TMP_PATH) && patch -p1 < $(HI3516DV300_PATCH_FILE)
-	$(hide) cp -rf $(KERNEL_CONFIG_PATH)/. $(KERNEL_SRC_TMP_PATH)/
-	$(hide) $(KERNEL_MAKE) -C $(KERNEL_SRC_TMP_PATH) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) distclean
-	$(hide) $(KERNEL_MAKE) -C $(KERNEL_SRC_TMP_PATH) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) hi3516dv300_emmc_smp_hos_l2_defconfig
-	$(hide) $(KERNEL_MAKE) -C $(KERNEL_SRC_TMP_PATH) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) -j64 uImage
-
-else ifeq ($(PRODUCT_NAME), hi3516dv300)
+ifneq ($(findstring $(PRODUCT_NAME), hi3516dv300 Hi3516DV300),)
 HI3516DV300_PATCH_DIR := $(OHOS_BUILD_HOME)/kernel/linux/patches/linux-4.19/hi3516dv300_small_patch
 HI3516DV300_PATCH_FILE := $(HI3516DV300_PATCH_DIR)/hi3516dv300_small.patch
 HDF_PATCH_FILE := $(HI3516DV300_PATCH_DIR)/hdf.patch
 DFX_PATCH_FILE := $(HI3516DV300_PATCH_DIR)/dfx.patch
 KERNEL_IMAGE_FILE := $(KERNEL_SRC_TMP_PATH)/arch/arm/boot/uImage
+ifeq ($(PRODUCT_NAME), hi3516dv300)
+DEFCONFIG_FILE := hi3516dv300_small_defconfig
+else
+DEFCONFIG_FILE := hi3516dv300_standard_defconfig
+endif
 export HDF_PROJECT_ROOT=$(OHOS_BUILD_HOME)/
 export PRODUCT_PATH=vendor/hisilicon/hispark_taurus_linux
 
@@ -93,7 +80,7 @@ $(KERNEL_IMAGE_FILE):
 	$(hide) cd $(KERNEL_SRC_TMP_PATH) && patch -p1 < $(DFX_PATCH_FILE) && patch -p1 < $(HDF_PATCH_FILE) && patch -p1 < $(HI3516DV300_PATCH_FILE)
 	$(hide) cp -rf $(KERNEL_CONFIG_PATH)/. $(KERNEL_SRC_TMP_PATH)/
 	$(hide) $(KERNEL_MAKE) -C $(KERNEL_SRC_TMP_PATH) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) distclean
-	$(hide) $(KERNEL_MAKE) -C $(KERNEL_SRC_TMP_PATH) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) hi3516dv300_small_defconfig
+	$(hide) $(KERNEL_MAKE) -C $(KERNEL_SRC_TMP_PATH) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) $(DEFCONFIG_FILE)
 	$(hide) $(KERNEL_MAKE) -C $(KERNEL_SRC_TMP_PATH) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) -j64 uImage
 endif
 .PHONY: build-kernel
