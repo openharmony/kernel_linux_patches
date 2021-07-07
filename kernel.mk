d8eff583fcf7839e2562e275444d1cfc9ebae085
Zachery Wu <wuzuoyu@huawei.com>
2021-07-07 12:30:47 +0800

    driver: add hilog and hievent buffer management drivers
    Signed-off-by: Zachery Wu <wuzuoyu@huawei.com>
    Change-Id: I9d7cbaa3c4687bdcada0499bd6509ba301926d0e

--- kernel.mk
+++ kernel.mk
@@ -63,7 +63,6 @@ ifneq ($(findstring $(PRODUCT_NAME), hi3516dv300 Hi3516DV300),)
 HI3516DV300_PATCH_DIR := $(OHOS_BUILD_HOME)/kernel/linux/patches/linux-4.19/hi3516dv300_small_patch
 HI3516DV300_PATCH_FILE := $(HI3516DV300_PATCH_DIR)/hi3516dv300_small.patch
 HDF_PATCH_FILE := $(HI3516DV300_PATCH_DIR)/hdf.patch
-DFX_PATCH_FILE := $(HI3516DV300_PATCH_DIR)/dfx.patch
 KERNEL_IMAGE_FILE := $(KERNEL_SRC_TMP_PATH)/arch/arm/boot/uImage
 ifeq ($(PRODUCT_NAME), hi3516dv300)
 DEFCONFIG_FILE := hi3516dv300_small_defconfig
@@ -76,7 +75,7 @@ export PRODUCT_PATH=vendor/hisilicon/hispark_taurus_linux
 $(KERNEL_IMAGE_FILE):
 	$(hide) echo "build kernel..."
 	$(hide) rm -rf $(KERNEL_SRC_TMP_PATH);mkdir -p $(KERNEL_SRC_TMP_PATH);cp -arfL $(KERNEL_SRC_PATH)/* $(KERNEL_SRC_TMP_PATH)/
-	$(hide) cd $(KERNEL_SRC_TMP_PATH) && patch -p1 < $(DFX_PATCH_FILE) && patch -p1 < $(HDF_PATCH_FILE) && patch -p1 < $(HI3516DV300_PATCH_FILE)
+	$(hide) cd $(KERNEL_SRC_TMP_PATH) && patch -p1 < $(HDF_PATCH_FILE) && patch -p1 < $(HI3516DV300_PATCH_FILE)
 	$(hide) cp -rf $(KERNEL_CONFIG_PATH)/. $(KERNEL_SRC_TMP_PATH)/
 	$(hide) $(KERNEL_MAKE) -C $(KERNEL_SRC_TMP_PATH) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) distclean
 	$(hide) $(KERNEL_MAKE) -C $(KERNEL_SRC_TMP_PATH) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) $(DEFCONFIG_FILE)
